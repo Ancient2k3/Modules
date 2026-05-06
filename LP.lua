8b1479f3c18de4df1894d6f8d4dc0745f5417d98
DE <trinhro2022@gmail.com>
2026-05-06 16:05:47 +0700

    Improve function formatting and readability in LP.lua
    Refactor functions for better readability and formatting.

--- LP.lua
+++ LP.lua
@@ -1,13 +1,22 @@
-function v1(name) return game:GetService(name) end
-local ws, plrs
+function v1(name)
+  return game:GetService(name)
+end local ws, plrs
 ws = v1"Workspace"
 plrs = v1"Players"
 
 local plr
 plr = plrs.LocalPlayer
 
-function get_root() return plr and plr.Character and plr.Character:FindFirstChild("HumanoidRootPart") end
-function is_alive() return plr and plr.Character and plr.Character:FindFirstChildOfClass("Humanoid") and plr.Character.Humanoid.Health > 0 end
+function get_root()
+  return plr and plr.Character
+  and plr.Character:FindFirstChild("HumanoidRootPart")
+end
+
+function is_alive()
+  return plr and plr.Character
+  and plr.Character:FindFirstChildOfClass("Humanoid")
+  and plr.Character.Humanoid.Health > 0
+end
 
 local module = {}
 
@@ -20,3 +29,4 @@ end
 
 return module
 -- Github bug suck --
+print"[lp module loaded... ]"

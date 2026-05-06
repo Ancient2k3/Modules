882c6b3b8c821547e25614b40993a4cb73f92e4d
DE <trinhro2022@gmail.com>
2026-05-06 16:03:37 +0700

    Refactor get_root and is_alive functions

--- LP.lua
+++ LP.lua
@@ -6,15 +6,17 @@ plrs = v1"Players"
 local plr
 plr = plrs.LocalPlayer
 
-function get_root()
-  return plr and plr.Character
-  and plr.Character:FindFirstChild("HumanoidRootPart")
-end
-
-function is_alive()
-  return plr and plr.Character
-  and plr.Character:FindFirstChildOfClass("Humanoid")
-  and plr.Character.Humanoid.Health > 0
-end
+function get_root() return plr and plr.Character and plr.Character:FindFirstChild("HumanoidRootPart") end
+function is_alive() return plr and plr.Character and plr.Character:FindFirstChildOfClass("Humanoid") and plr.Character.Humanoid.Health > 0 end
 
 local module = {}
+
+module.tp = function(pos)
+  local hrp = get_root()
+  if hrp and is_alive() then
+    hrp.CFrame = CFrame.new(pos)
+  end
+end
+
+return module
+-- Github bug suck --
